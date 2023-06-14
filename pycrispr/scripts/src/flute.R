54c96130c8aefca1bcabc55d647eb0c3a38c10f1
niekwit <41302859+niekwit@users.noreply.github.com>
2023-06-14 13:01:01 +0100

    first commit

---
+++ pycrispr/scripts/src/flute.R
@@ -0,0 +1,70 @@
+library(MAGeCKFlute)
+library(clusterProfiler)
+library(ggplot2)
+
+#path to gene summary file 
+file1 = snakemake@input[[1]]
+
+#path to sgRNA summary file 
+file2 = snakemake@input[[2]]
+
+#load data
+gdata <- ReadRRA(file1)
+gdata$LogFDR <- -log10(gdata$FDR)
+
+#volcano plot
+pdf(snakemake@output[[1]])
+VolcanoView(gdata, x = "Score", y = "FDR", Label = "id")
+dev.off()
+
+#dot plots
+gdata$RandomIndex <- sample(1:nrow(gdata), nrow(gdata))
+gdata <- gdata[order(-gdata$Score), ]
+
+gg <- gdata[gdata$Score > 0, ]
+pdf(snakemake@output[[2]])
+ScatterView(gg, x = "RandomIndex", y = "Score", label = "id",
+                 y_cut = CutoffCalling(gdata$Score,2), 
+                 groups = "top", top = 10, ylab = "Log2FC")
+dev.off()
+
+gg <- gdata[gdata$Score < 0, ]
+pdf(snakemake@output[[3]])
+ScatterView(gg, x = "RandomIndex", y = "Score", label = "id",
+                 y_cut = CutoffCalling(gdata$Score,2), 
+                 groups = "bottom", top = 10, ylab = "Log2FC")
+dev.off()
+
+#sgRNA rank plot
+sdata <- ReadsgRRA(file2)
+
+pdf(snakemake@output[[4]])
+sgRankView(sdata, top = 5, bottom = 5)
+dev.off()
+
+#enrichment analysis
+geneList <- gdata$Score
+names(geneList) <- gdata$id
+
+enrich_pos <- EnrichAnalyzer(geneList = geneList[geneList > 0.5], 
+                            method = "GSEA", 
+                            type = "KEGG",
+                            organism = snakemake@params[[1]],
+                            filter = TRUE)
+
+pdf(snakemake@output[[5]])
+EnrichedView(enrich_pos, mode = 1, top = 10, bottom = 0)
+dev.off()
+
+enrich_neg <- EnrichAnalyzer(geneList = geneList[geneList < -0.5], 
+                            method = "GSEA", 
+                            type = "KEGG",
+                            organism = snakemake@params[[1]],
+                            filter = TRUE)
+
+pdf(snakemake@output[[6]])
+EnrichedView(enrich_neg, mode = 1, top = 0, bottom = 10)
+dev.off()
+
+
+
